9050db45c5daeda5cb91e36b7218250c4031962a
Khaleel Khan <tokhaleelkhan@gmail.com>
2019-06-17 11:06:17 +0200

    added documentation for progress display

--- CoverletterCreator/ProgressDisplay.py
+++ CoverletterCreator/ProgressDisplay.py
@@ -1,16 +1,27 @@
-from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtCore import QSettings
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 from CoverletterCreator.ui import progress
-from PyQt5 import QtCore
-
 
 
 class ProgressDisplay(QtWidgets.QDialog, progress.Ui_ProgressDialog):
+	"""
+	This class handles execution of command while showing the log in a dialog window.
+	"""
 	def __init__(self, executable, arguments, parent=None):
+		"""
+		Initialising ProgressDisplay.
+
+		:param executable: Executable command to run.
+		:type executable: str
+		:param arguments: List of arguments passed to Qprocess
+		:type arguments: list of str
+		:param parent: Optional Qwidget parent
+		:type parent: Qwidget
+		"""
 		super(ProgressDisplay, self).__init__(parent)
 		self.setupUi(self)
+
+		# start the progress animation
 		movie = QtGui.QMovie(":/animation/ajax-loader.gif")
 		self.progress_bar.setMovie(movie)
 		movie.start()
@@ -20,7 +31,6 @@ class ProgressDisplay(QtWidgets.QDialog, progress.Ui_ProgressDialog):
 		# QProcess emits `readyRead` when there is data to be read
 		self.process.readyRead.connect(self.dataReady)
 
-		# Just to prevent accidentally running multiple times
 		# Disable the button when process starts, and enable it when it finishes
 		self.process.started.connect(lambda: self.buttonBox.setEnabled(False))
 		self.process.finished.connect(self.processFinished)
@@ -28,17 +38,25 @@ class ProgressDisplay(QtWidgets.QDialog, progress.Ui_ProgressDialog):
 		self.process.start(executable, arguments)
 
 	def dataReady(self):
+		"""
+		Called by Qprocess on readyData. Adds output of Qprocess to PlainTextEdit.
+
+		"""
 		cursor = self.log_display.textCursor()
 		cursor.movePosition(cursor.End)
 		cursor.insertText(str(self.process.readAll(), 'utf-8'))
 		self.log_display.ensureCursorVisible()
 
 	def processFinished(self):
+		"""
+		Called in Qprocess finished. Stop movie and display "finished".
+		Also enables Ok button for closing this dialog.
+
+		"""
 		self.buttonBox.setEnabled(True)
 		self.label.setText("Compilation Finished")
 		self.progress_bar.movie().stop()
 		self.close()
-
 
 
 def run():

3f8bd4533d2a2b0873dc495c411f69b8a8eeffb0
Khaleel Khan <tokhaleelkhan@gmail.com>
2019-06-29 14:01:00 +0200

    fix indentation

--- CoverletterCreator/SpellTextEdit.py
+++ CoverletterCreator/SpellTextEdit.py
@@ -19,132 +19,129 @@ from spellchecker import SpellChecker
 
 class SpellTextEdit(QPlainTextEdit):
 
-    def __init__(self, *args):
-        QPlainTextEdit.__init__(self, *args)
+	def __init__(self, *args):
+		QPlainTextEdit.__init__(self, *args)
 
-        # Path for custom dict
-        custom_dict_path = QStandardPaths.writableLocation(QStandardPaths.AppDataLocation)
+		# Path for custom dict
+		custom_dict_path = QStandardPaths.writableLocation(QStandardPaths.AppDataLocation)
 		custom_dict_filename = 'customWords.txt'
-        self.custom_dict = os.path.join(custom_dict_path, custom_dict_filename)
-        os.makedirs(os.path.dirname(self.custom_dict), exist_ok=True)
+		self.custom_dict = os.path.join(custom_dict_path, custom_dict_filename)
+		os.makedirs(os.path.dirname(self.custom_dict), exist_ok=True)
 
-        # Default dictionary based on the local locale.
+		# Default dictionary based on the local locale.
 		self.spell = SpellChecker()
 		if os.path.exists(self.custom_dict):
 			self.spell.word_frequency.load_text_file(self.custom_dict)
 
-        self.highlighter = Highlighter(self.document())
+		self.highlighter = Highlighter(self.document())
 		self.highlighter.setDict(self.spell)
 
+	def mousePressEvent(self, event):
+		if event.button() == Qt.RightButton:
+			# Rewrite the mouse event to a left button event so the cursor is
+			# moved to the location of the pointer.
+			event = QMouseEvent(QEvent.MouseButtonPress, event.pos(),
+								Qt.LeftButton, Qt.LeftButton, Qt.NoModifier)
+		QPlainTextEdit.mousePressEvent(self, event)
 
-    def mousePressEvent(self, event):
-        if event.button() == Qt.RightButton:
-            # Rewrite the mouse event to a left button event so the cursor is
-            # moved to the location of the pointer.
-            event = QMouseEvent(QEvent.MouseButtonPress, event.pos(),
-                Qt.LeftButton, Qt.LeftButton, Qt.NoModifier)
-        QPlainTextEdit.mousePressEvent(self, event)
+	def contextMenuEvent(self, event):
+		popup_menu = self.createStandardContextMenu()
 
-    def contextMenuEvent(self, event):
-        popup_menu = self.createStandardContextMenu()
+		# Select the word under the cursor.
+		cursor = self.textCursor()
+		cursor.select(QTextCursor.WordUnderCursor)
+		self.setTextCursor(cursor)
 
-        # Select the word under the cursor.
-        cursor = self.textCursor()
-        cursor.select(QTextCursor.WordUnderCursor)
-        self.setTextCursor(cursor)
-
-        # Check if the selected word is misspelled and offer spelling
-        # suggestions if it is.
-        if self.textCursor().hasSelection():
-            text = str(self.textCursor().selectedText())
+		# Check if the selected word is misspelled and offer spelling
+		# suggestions if it is.
+		if self.textCursor().hasSelection():
+			text = str(self.textCursor().selectedText())
 			if text not in self.spell:
-                spell_menu = QMenu('Spelling Suggestions')
+				spell_menu = QMenu('Spelling Suggestions')
 				for word in self.spell.candidates(text):
-                    action = SpellAction(word, spell_menu)
-                    action.correct.connect(self.correctWord)
-                    spell_menu.addAction(action)
-                spell_menu.addSeparator()
-                action = spell_menu.addAction('Add to Dictionary')
-                action.triggered.connect(lambda: self.addWord(text))
-                # Only add the spelling suggests to the menu if there are
-                # suggestions.
-                if len(spell_menu.actions()) != 0:
-                    popup_menu.insertSeparator(popup_menu.actions()[0])
-                    popup_menu.insertMenu(popup_menu.actions()[0], spell_menu)
+					action = SpellAction(word, spell_menu)
+					action.correct.connect(self.correctWord)
+					spell_menu.addAction(action)
+				spell_menu.addSeparator()
+				action = spell_menu.addAction('Add to Dictionary')
+				action.triggered.connect(lambda: self.addWord(text))
+				# Only add the spelling suggests to the menu if there are
+				# suggestions.
+				if len(spell_menu.actions()) != 0:
+					popup_menu.insertSeparator(popup_menu.actions()[0])
+					popup_menu.insertMenu(popup_menu.actions()[0], spell_menu)
 
-        popup_menu.exec_(event.globalPos())
+		popup_menu.exec_(event.globalPos())
 
-    def correctWord(self, word):
-        '''
-        Replaces the selected text with word.
-        '''
-        cursor = self.textCursor()
-        cursor.beginEditBlock()
+	def correctWord(self, word):
+		'''
+		Replaces the selected text with word.
+		'''
+		cursor = self.textCursor()
+		cursor.beginEditBlock()
 
-        cursor.removeSelectedText()
-        cursor.insertText(word)
+		cursor.removeSelectedText()
+		cursor.insertText(word)
 
-        cursor.endEditBlock()
+		cursor.endEditBlock()
 
-    def addWord(self, word):
+	def addWord(self, word):
 		self.spell.word_frequency.add(word)
 		self.highlighter.setDict(self.spell)
-        self.highlighter.rehighlight()
+		self.highlighter.rehighlight()
 
 		# Save custom word
 		with open(self.custom_dict, 'a+') as f:
 			f.write(word + " ")
 
 class Highlighter(QSyntaxHighlighter):
+	WORDS = u'(?iu)[\w\']+'
 
-    WORDS = u'(?iu)[\w\']+'
+	def __init__(self, *args):
+		QSyntaxHighlighter.__init__(self, *args)
 
-    def __init__(self, *args):
-        QSyntaxHighlighter.__init__(self, *args)
+		self.dict = None
 
-        self.dict = None
+	def setDict(self, dict):
+		self.dict = dict
 
-    def setDict(self, dict):
-        self.dict = dict
+	def highlightBlock(self, text):
+		if not self.dict:
+			return
 
-    def highlightBlock(self, text):
-        if not self.dict:
-            return
+		text = str(text)
 
-        text = str(text)
+		format = QTextCharFormat()
+		format.setUnderlineColor(Qt.red)
+		format.setUnderlineStyle(QTextCharFormat.SpellCheckUnderline)
 
-        format = QTextCharFormat()
-        format.setUnderlineColor(Qt.red)
-        format.setUnderlineStyle(QTextCharFormat.SpellCheckUnderline)
-
-        for word_object in re.finditer(self.WORDS, text):
+		for word_object in re.finditer(self.WORDS, text):
 			if word_object.group() not in self.dict:
-                self.setFormat(word_object.start(),
-                    word_object.end() - word_object.start(), format)
+				self.setFormat(word_object.start(),
+							   word_object.end() - word_object.start(), format)
 
 
 class SpellAction(QAction):
+	'''
+	A special QAction that returns the text in a signal.
+	'''
 
-    '''
-    A special QAction that returns the text in a signal.
-    '''
+	correct = pyqtSignal(str)
 
-    correct = pyqtSignal(str)
+	def __init__(self, *args):
+		QAction.__init__(self, *args)
 
-    def __init__(self, *args):
-        QAction.__init__(self, *args)
-
-        self.triggered.connect(lambda x: self.correct.emit(
-            str(self.text())))
+		self.triggered.connect(lambda x: self.correct.emit(
+			str(self.text())))
 
 
 def main(args=sys.argv):
-    app = QApplication(args)
+	app = QApplication(args)
 
-    spellEdit = SpellTextEdit()
-    spellEdit.show()
+	spellEdit = SpellTextEdit()
+	spellEdit.show()
 
-    return app.exec_()
+	return app.exec_()
 
 if __name__ == '__main__':
-    sys.exit(main())
+	sys.exit(main())
